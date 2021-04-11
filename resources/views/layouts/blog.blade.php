8b08b627ed7f1d428a9b4d16afabdd7db5d9e274
thfProjects <nikola.djurovic997@gmail.com>
2021-04-11 22:23:36 +0200

    Made blog post view more responsive

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -1,11 +1,13 @@
-<div class="card my-3 blog">
-    <div class="row no-gutters h-100">
-        <div class="col-md-4 h-100">
+<div class="card my-3">
+    <div class="row no-gutters">
+        <div class="col-md-4 blog-height">
             <img class="card-img-left" src="{{ asset('storage/images/'.$blog->image) }}" alt="{{ __('Image') }}">
         </div>
-        <div class="col-md-8 h-100">
-            <div class="card-body">
-                <h3><a class="card-title nav-link px-0 py-0 d-inline" href="/blogs/{{ $blog->id }}">{{ $blog->title }}</a></h3>
+        <div class="col-md-8 blog-max-height">
+            <div class="card-body">     
+                <div class="blog-title">
+                    <a class="card-title nav-link px-0 py-0 d-inline" href="/blogs/{{ $blog->id }}">{{ $blog->title }}</a>
+                </div>          
                 <p class="card-text">{{ __('By ') }}<a class="nav-link px-0 py-0 d-inline" href="/users/{{ $blog->user_id }}">{{ $blog->author }}</a></p>
             </div>
         </div>
